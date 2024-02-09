b2888b6ebc8f6ad5bddaa61f2ce12d10a0ca35f4
Yoshiteru Nagata <yositeru1108@nagoya-u.jp>
2024-02-09 13:50:21 +0900

    update proto, msg format

--- train-operator.go
+++ train-operator.go
@@ -31,9 +31,13 @@ func init() {
 
 func supplyRecommendCallback(clt *sxutil.SXServiceClient, sp *api.Supply) {
 	recommend := &rcm.Recommend{}
-	err := proto.Unmarshal(sp.Cdata.Entity, recommend)
-	if err == nil {
-		log.Printf("Received Recommend Supply from %d %+v", sp.SenderId, recommend)
+	if sp.Cdata != nil {
+		err := proto.Unmarshal(sp.Cdata.Entity, recommend)
+		if err == nil {
+			log.Printf("Received Recommend Supply from %d %+v", sp.SenderId, recommend)
+		}
+	} else {
+		log.Printf("Received JsonRecord Supply from %d %+v", sp.SenderId, sp.ArgJson)
 	}
 }
 
@@ -115,5 +119,33 @@ func main() {
 	log.Print("Subscribe Supply")
 	go subscribeRecommendSupply(rcmClient)
 	// go subscribeJsonRecordSupply(envClient)
+
+	// タイマーを開始する
+	ticker := time.NewTicker(15 * time.Second)
+	defer ticker.Stop()
+
+	// 現在時刻を取得し、次の実行時刻まで待機する
+	start := time.Now()
+	adjust := start.Truncate(15 * time.Second).Add(15 * time.Second)
+	time.Sleep(adjust.Sub(start))
+
+	for {
+		select {
+		case t := <-ticker.C:
+			// ここに実行したい処理を書く
+			fmt.Println("実行時刻:", t.Format("15:04:05"))
+			smo := sxutil.SupplyOpts{
+				Name: role,
+				JSON: fmt.Sprintf("{\"%s\": \"chincha\"}", role), // ここにバス運行状況を入れる
+			}
+			_, nerr := rcmClient.NotifySupply(&smo)
+			if nerr != nil {
+				log.Printf("Send Fail! %v\n", nerr)
+			} else {
+				//							log.Printf("Sent OK! %#v\n", ge)
+			}
+		}
+	}
+
 	wg.Wait()
 }
